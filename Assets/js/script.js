0380a8c9be69e87695792474e92293c8c1e56723
jaimepreuss <jaimepreuss@gmail.com>
2020-05-14 21:34:01 -0700

    updated index.html and quiz.js, changed homepage id's of form, added eventlisteners to form elements on quiz.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,13 @@
 //variables
 const start = document.getElementById('start');
 const prev = document.getElementById('previous');
+//change
 const continue = document.getElementById('continue');
-var weight = 85
+var weight = 85;
+var userSex = $("#userFormWeight").on("click", function() {
+    userSex = userSex;
+});
+
 //ajax call for recipes api
 fetch("https://edamam-recipe-search.p.rapidapi.com/search?q=" + var1, {
     "method": "GET",
@@ -20,7 +25,7 @@ fetch("https://edamam-recipe-search.p.rapidapi.com/search?q=" + var1, {
     });
 
 //ajax call for BMI
-function(weight) {
+function bmiCall(weight) {
     var settings = {
         "async": true,
         "crossDomain": true,
@@ -33,11 +38,12 @@ function(weight) {
             "accept": "application/json"
         },
         "processData": false,
-        "data": "{\"weight\":{\"value\":\"85.00\",\"unit\":\"lb\"},\"height\":{\"value\":\"170.00\",\"unit\":\"cm\"},\"sex\":\"m\",\"age\":\"24\",\"waist\":\"34.00\",\"hip\":\"40.00\"}"
+        "data": "{\"weight\":{\"value\":\"85.00\",\"unit\":\"lb\"},\"height\":{\"value\":\"170.00\",\"unit\":\"cm\"},\"" + userSex +"\":\"m\",\"age\":\"24\",\"waist\":\"34.00\",\"hip\":\"40.00\"}"
     }
     
     $.ajax(settings).done(function (response) {
         console.log(response);
+        //taking infomation -> storing it -> showing it.
     });
 }
 
